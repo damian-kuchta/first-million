161cefc2d2c5eac36c3e083c3c71c6547d5e87b8
Damian Kuchta <damianes1994@gmail.com>
2022-04-23 01:55:26 +0200

    added saving results to png

--- src/components/Result/ResultControl/ResultControlPanel.js
+++ src/components/Result/ResultControl/ResultControlPanel.js
@@ -1,6 +1,7 @@
 import resultImg from '../../../assets/img/result-img.png';
 import classes from './ResultControlPanel.module.css';
 import Button from '../../UI/Button';
+import html2canvas from "html2canvas";
 
 const ResultControlPanel = (props) => {
 
@@ -8,6 +9,23 @@ const ResultControlPanel = (props) => {
     props.showResult(false);
     props.showInitialPage(true);
   }
+
+  const saveResultImg = (()=> {
+    html2canvas(document.querySelector("body")).then(canvas => {
+        if(window.navigator.msSaveBlob) {
+            window.navigator.msSaveBlob(canvas.msToBlob(),"zarobki.png")
+        }
+        else {
+            const a = document.createElement('a');
+            document.body.appendChild(a);
+            a.href = canvas.toDataURL();
+            a.download = 'twoje_zarobki.png';
+            a.click();
+            document.body.removeChild(a);
+        }
+    });
+});
+
 
   return (
     <div className={classes['result-controls']}>
@@ -19,7 +37,7 @@ const ResultControlPanel = (props) => {
         />
       </div>
       <div className={classes['result-controls__btn-wrap']} data-html2canvas-ignore>
-        <Button className={`${classes['result-controls__btn']} ${classes['result-controls__btn--black']}`} disabled>Pobierz PNG</Button>
+        <Button onClick={saveResultImg} className={`${classes['result-controls__btn']} ${classes['result-controls__btn--black']}`} disabled={false}>Pobierz PNG</Button>
         <Button onClick={appViewStateHandler} className={`${classes['result-controls__btn']} ${classes['result-controls__btn--close']}`}>Zamknij!</Button>
       </div>
     </div>
